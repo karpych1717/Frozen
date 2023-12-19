11effc7b8543cd2c01a1a306ce9849edf51f349d
PeterMoroz17 <peter.moroz17@gmail.com>
2023-12-19 19:13:07 +0200

    some fixes

--- hometask5/cannon.js
+++ hometask5/cannon.js
@@ -7,6 +7,7 @@ class List {
     this.output = 0
     this.len = 1
   }
+
   unshift (num) {
     if (this.input === this.len) {
       if (this.output !== 0) {
@@ -27,6 +28,7 @@ class List {
     this.arr[this.input] = num
     this.input++
   }
+  
   pop () {
     this.output++
     return this.arr[this.output-1]
@@ -35,15 +37,22 @@ class List {
 
 
 const magazine = new List()
-const MASS = 10, G = 9.8, HEIGHT = 500, WIDTH = 750, VELOSITY = 270
+const MASS = 10
+const G = 9.8
+const HEIGHT = 500
+const WIDTH = 750
+const VELOSITY = 270
 let target = WIDTH/2
+let reloadtime = 300
+
 let angle = new Array(100)
 let timer = new Array(100)
-let length = 0
 let waiting = 0
+let length = 0
 let flying = 0
 let lasttime = -1
-let reloadtime = 300
+let xpos
+let ypos
 
 const { cvs, ctx } = canvasSetUp()
 cvs.addEventListener('pointerdown', handleClick)
@@ -54,7 +63,7 @@ function handleClick (event) {
 }
 requestAnimationFrame(render)
 
-function draw (x, y, r) {
+function circle (x, y, r) {
   ctx.lineWidth = 1
   ctx.strokeStyle = 'white'
   ctx.beginPath()
@@ -66,9 +75,9 @@ function render (time) {
   if (flying !== 0) {
     ctx.clearRect(0, 0, WIDTH, HEIGHT)
   }
-  draw(target, HEIGHT, 25)
+  circle(target, HEIGHT, 25)
   
-  if (Math.floor(time/reloadtime) !== lasttime && waiting !== 0) {
+  if (Math.floor(time / reloadtime) !== lasttime && waiting !== 0) {
     angle[length] = magazine.pop()
     timer[length] = time
     length++
@@ -78,10 +87,10 @@ function render (time) {
   }
 
   if (length > 30) {
-    angle2 = new Array(100)
-    timer2 = new Array(100)
+    let angle2 = new Array(100)
+    let timer2 = new Array(100)
     let length2 = 0
-    for (let i=0; i<length; i++) {
+    for (let i = 0; i < length; i++) {
       if (timer[i] !== -1) {
         angle2[length2] = angle[i]
         timer2[length2] = timer[i]
@@ -93,11 +102,11 @@ function render (time) {
     length = length2
   }
 
-  for (let i=0; i<length; i++) {
+  for (let i = 0; i < length; i++) {
     if (timer[i] !== -1) {
-      let xpos = VELOSITY * Math.cos(angle[i]) * (time - timer[i]) / 1000
-      let ypos = -10 + HEIGHT - VELOSITY * Math.sin(angle[i]) * (time - timer[i]) / 1000 + G * MASS * (time - timer[i]) ** 2 / 2000000
-      draw(xpos, ypos, 10)
+      xpos = VELOSITY * Math.cos(angle[i]) * (time - timer[i]) / 1000
+      ypos = -10 + HEIGHT - VELOSITY * Math.sin(angle[i]) * (time - timer[i]) / 1000 + G * MASS * (time - timer[i]) ** 2 / 2000000
+      circle(xpos, ypos, 10)
 
       if (HEIGHT - ypos <= 35 && Math.abs(target - xpos) <= 35) {
         console.log("reposition proposal")
@@ -114,15 +123,25 @@ function render (time) {
 }
 
 function solve() {
-  for (let i=0; i<Math.PI; i+=0.0001) {
-    let t = target/VELOSITY/Math.cos(i)
-    if (Math.abs(VELOSITY*Math.sin(i)*t - G * MASS * (t ** 2) / 2) < 1) {
-      for (let j=0; j<20; j+=0.01) {
-        let xpos = VELOSITY * Math.cos(i) * j
-        let ypos = -10 + HEIGHT - VELOSITY * Math.sin(i) * j + G * MASS * j ** 2 / 2
-        draw(xpos, ypos, 1)
+  let t
+  for (let i = 0; i < Math.PI; i += 0.0001) {
+    t = target / VELOSITY / Math.cos(i)
+    if (Math.abs(VELOSITY * Math.sin(i) * t - G * MASS * (t ** 2) / 2) < 1) {
+      ctx.lineWidth = 2
+      ctx.strokeStyle = 'white'
+      ctx.beginPath()
+      ctx.moveTo(0, HEIGHT-10)
+      for (let j = 0; j < 20; j += 0.01) {
+        xpos = VELOSITY * Math.cos(i) * j
+        ypos = -10 + HEIGHT - VELOSITY * Math.sin(i) * j + G * MASS * j ** 2 / 2
+        if (ypos > 490) break
+        ctx.lineTo(xpos, ypos)
       }
-      draw(target / 4, HEIGHT - Math.tan(i) * target / 4, 5)
+      ctx.stroke()
+      t=2
+      while (HEIGHT - 5 < Math.tan(i) * target / t) t+=1
+      console.log(t)
+      circle(target / t, HEIGHT - Math.tan(i) * target / t, 5)
     }
   }
 }
