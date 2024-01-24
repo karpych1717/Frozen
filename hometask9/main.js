b5e9fcbcb870842d5838b583b8bb39199a9f804c
karpych1717 <admkarpych1717@gmail.com>
2024-01-24 19:49:04 +0200

    init task

---
+++ hometask9/main.js
@@ -0,0 +1,42 @@
+/* global log */
+'use strict'
+
+const d = 'hebisaddpvjsowenovpoajfaonvsm'
+const a = 'add'
+
+const i = 0
+const j = 0
+
+let result
+const current = q0 // rename and replace
+const isDoing = false
+
+while (isDoing) {
+  current()
+}
+
+log(result || 'not found')
+
+function q0 () {
+
+}
+
+function q1 () {
+
+}
+
+function q2 () {
+
+}
+
+function q3 () {
+
+}
+
+function q4 () {
+
+}
+
+function q5 () {
+
+}
