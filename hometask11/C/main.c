318b47b6a3de6bcaadef402e22cefe117f061e75
karpych1717 <admkarpych1717@gmail.com>
2024-02-21 20:59:54 +0200

    make task prepeared

--- hometask11/C/main.c
+++ hometask11/C/main.c
@@ -1,27 +1,36 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-void diver (int *numbers, int length, int *result, int dive);
+#include "blackBox.h"
 
-int main () {
-  int length = 5;
-  int numbers[5];
 
-  for (int i = 0; i < length; i++) {
-    numbers[i] = i + 1;
-  }
+void dive (int *numbers, int length, int *result, int dive);
 
-  int dive;
-  int *result = malloc(length * sizeof (int));
 
-  for (int i = 0; i < length; i++) {
-    result[0] = numbers[i];
-    diver(numbers, length, result, 0);
-  }
+int main (void)
+{
+    int length;
+    int *numbers = NULL;
+    int *result = NULL;
 
-  free(result);
-return 0;
+    prepare(&length, &numbers);
+    result = malloc(length * sizeof (int));
+
+
+    for (int i = 0; i < length; i++)
+    {
+        result[0] = numbers[i];
+        dive(numbers, length, result, 0);
+    }
+
+    free(result);
+    return 0;
 }
 
-void diver (int *numbers, int length, int *result, int dive) {
 
+void dive (int *numbers, int length, int *result, int depth)
+{
+    printf("dive (depth = %d) occures here.\n", depth);
+    printf("current numbers array:\n");
+    printArray(numbers, length); // for printing array "numbers" or "result"
 }
