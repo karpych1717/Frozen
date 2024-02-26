b2c8f346e5adca8b696bc5f324ab4b4fdf55c2b1
PeterMoroz17 <peter.moroz17@gmail.com>
2024-02-26 19:01:32 +0200

    recursion

--- hometask11/C/main.c
+++ hometask11/C/main.c
@@ -2,9 +2,23 @@
 #include <stdio.h>
 
 #include "blackBox.h"
+int check(int number, int *result, int depth)
+{
+    for (int i = 0; i < depth; i++) if (number == result[i]) return 1;
+    return 0;
+}
 
-
-void dive (int *numbers, int length, int *result, int dive);
+void dive (int *numbers, int length, int *result, int depth)
+{
+    if (depth < length) {
+        for (int i = 0; i < length; i++) {
+            if (check(numbers[i], result, depth) == 0) {
+                result[depth] = numbers[i];
+                dive(numbers, length, result, depth + 1);
+            }
+        }
+    } else printArray(result, length);
+}
 
 
 int main (void)
@@ -20,18 +34,10 @@ int main (void)
     for (int i = 0; i < length; i++)
     {
         result[0] = numbers[i];
-        dive(numbers, length, result, 0);
+        dive(numbers, length, result, 1);
     }
 
     free(numbers);
     free(result);
     return 0;
-}
-
-
-void dive (int *numbers, int length, int *result, int depth)
-{
-    printf("dive (depth = %d) occures here.\n", depth);
-    printf("current numbers array:\n");
-    printArray(numbers, length); // for printing array "numbers" or "result"
 }
