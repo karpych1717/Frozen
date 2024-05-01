aee6ca0e36b08fc0817fd94a8ea3553c97c6f6a9
karpych1717 <admkarpych1717@gmail.com>
2024-05-01 19:35:52 +0300

    core created

--- short/hometask17/C/main.c
+++ short/hometask17/C/main.c
@@ -5,40 +5,87 @@
 typedef struct equasion
 {
     int n;
+    float** matrix;
+} Equasion;
+
+typedef struct dots
+{
+    int n;
     float* x;
     float* y;
-} equasion;
+} Dots;
 
-equasion* getData()
+// prototypes here, a code is below the main
+Dots* getData();
+Equasion* setUp (Dots* dots);
+void solve (Equasion *eq);
+float interpolated (Equasion *solved, float argument);
+
+
+int main (void)
+{
+    Dots* dots = getData();
+    Equasion* eq = setUp(dots);
+
+    solve(eq);
+
+    // вивести в файл result.dat набір 100 точок з інтервалу
+    // циклом, з допомогою interpolated(eq, argument)
+    
+    return 0;
+}
+
+
+Dots* getData()
 {
     FILE *file;
     file = fopen("input.dat", "r");
     if (file == NULL) printf("file??");
 
-    int n;
-    fscanf(file, "%d", &n);
+    Dots* dots = malloc(sizeof(Dots));
+    fscanf(file, "%d", &(dots->n));
 
-    float* x = malloc(n * sizeof(float));
-    float* y = malloc(n * sizeof(float));
+    dots->x = malloc(dots->n * sizeof(float));
+    dots->y = malloc(dots->n * sizeof(float));
     
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < dots->n; i++)
     {
-        fscanf(file, "%f", &x[i]);
-        fscanf(file, "%f", &y[i]);
+        fscanf(file, "%f", &(dots->x[i]));
+        fscanf(file, "%f", &(dots->y[i]));
     }
-    
-    equasion* eq = malloc(sizeof(equasion));
 
-    eq->n = n;
-    eq->x = x;
-    eq->y = y;
+    return dots;
+}
+
+Equasion* setUp (Dots* dots)
+{
+    Equasion* eq = malloc(sizeof(Equasion));
+
+    eq->n = 2 * (dots->n - 1);
+    eq->matrix = malloc( eq->n * sizeof(float*));
+
+    for (int i = 0; i < eq->n; i++)
+    {
+        eq->matrix[i] = malloc(eq->n * sizeof(float*) + sizeof(float*));
+    }
+
+    //
+    //
 
     return eq;
 }
 
-int main (void)
+void solve (Equasion *eq)
 {
-    equasion* eq = getData();
-    
-    return 0;
+    //
+    //
+
+}
+
+float interpolated (Equasion *solved, float argument) {
+    float result;
+
+    //
+
+    return result;
 }
