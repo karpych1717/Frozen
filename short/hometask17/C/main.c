48e63eb2498be14222f002b352efc47eaddef06d
PeterMoroz17 <peter.moroz17@gmail.com>
2024-05-08 18:03:28 +0300

    17

--- short/hometask17/C/main.c
+++ short/hometask17/C/main.c
@@ -31,6 +31,15 @@ int main (void)
 
     // вивести в файл result.dat набір 100 точок з інтервалу
     // циклом, з допомогою interpolated(eq, argument)
+
+    FILE *result;
+    result = fopen("result.csv", "w");
+
+    fprintf(result, "x, y\n");
+    for (int i = -49; i <= 50; i++)
+    {
+        fprintf(result, "%f, %f\n", i, interpolated(eq, i));
+    }
     
     return 0;
 }
@@ -62,30 +71,62 @@ Equasion* setUp (Dots* dots)
     Equasion* eq = malloc(sizeof(Equasion));
 
     eq->n = 2 * (dots->n - 1);
-    eq->matrix = malloc( eq->n * sizeof(float*));
+    eq->matrix = malloc(eq->n * sizeof(float*));
 
     for (int i = 0; i < eq->n; i++)
     {
         eq->matrix[i] = malloc(eq->n * sizeof(float*) + sizeof(float*));
     }
 
-    //
-    //
+    float matrix[6][7] = {{dots->x[0] * dots->x[0],      dots->x[0],       1,                       0,               0,       0,      dots->y[0]},\
+                          {         2 * dots->x[1],               1,       0,         -2 * dots->x[1],              -1,       0,               0},\
+                          {dots->x[1] * dots->x[1],      dots->x[1],       1,                       0,               0,       0,      dots->y[1]},\
+                          {                      1,               0,       0,                      -1,               0,       0,               0},\
+                          {                      0,               0,       0, dots->x[1] * dots->x[1],      dots->x[1],       1,      dots->y[1]},\
+                          {                      0,               0,       0, dots->x[2] * dots->x[2],      dots->x[2],       1,      dots->y[2]}};
 
     return eq;
 }
 
 void solve (Equasion *eq)
 {
-    //
-    //
+    Equasion* eq2 = malloc(sizeof(Equasion));
+    eq2->n = eq->n;
+    eq2->matrix = eq->matrix;
+
+    float k;
+    for (int t = 0; t < eq2->n; t++)
+    {
+        for (int i = 0; i < eq2->n; i++)
+        {
+            if (i == t) continue;
+            k = eq2->matrix[i][t] / eq2->matrix[t][t];
+            for (int j = 0; j < eq2->n + 1; j++)
+            {
+                eq2->matrix[i][j] = eq2->matrix[i][j] - eq2->matrix[t][j] * k;
+            }
+        }
+    }
+
+    // O(n^3 - n)   :O
+
+    float* answer = malloc((eq2->n) * sizeof(float));
+    for (int i = 0; i < eq2->n; i++)
+    {
+        answer[i] = eq2->matrix[i][eq2->n] / eq2->matrix[i][i];
+    }
+    return answer;
 
 }
 
 float interpolated (Equasion *solved, float argument) {
-    float result;
+    float result = 0, arg = 1;
 
-    //
+    for (int i = solved->n - 1; i >= 0; i--)
+    {
+        result += solved->matrix[i][0] * arg;
+        arg *= argument;
+    }
 
     return result;
 }
