ea9690fc561dc583948c36791d2843b3cba7aaab
karpych1717 <admkarpych1717@gmail.com>
2024-09-26 10:25:20 +0300

    harder

--- short/hometask_vector/src/main.js
+++ short/hometask_vector/src/main.js
@@ -5,6 +5,18 @@ function log (something) {
   _textarea.value += '\n'
 }
 
+function vecLog (vector) {
+  if (vector instanceof Vector) {
+    log(vector.string)
+    log(vector.abs)
+    log(Math.round(vector.angle / Math.PI * 180))
+
+    return
+  }
+
+  throw new Error ('vector is not a Vector')
+}
+
 const vector1 = new Vector(1, 0, 'Cartesian')
 const vector2 = new Vector(1, Math.PI, 'Polar')
 
@@ -14,10 +26,11 @@ const vector4 = vector2.add(new Vector(-12, 0, 'Cartesian'))
 const MScalar = vector1.multyplyScalar(vector2)
 const MVector = vector1.multyplyVector(new Vector(0, 1, 'Cartesian'))
 
-log(vector1.string)
-log(vector2.string)
-log(vector3.string)
-log(vector4.string)
+;[vector1, vector2, vector3, vector4]
+  .forEach(vector => {
+    vecLog(vector)
+    log('\n')
+  })
 
 log(MScalar)
 log(MVector)
