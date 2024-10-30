5b639abc9755b88b350deea154281ad849938e83
PeterMoroz17 <peter.moroz17@gmail.com>
2024-10-30 18:20:49 +0200

    Reset button

--- long/least_action/main.js
+++ long/least_action/main.js
@@ -5,7 +5,9 @@ path.fillRandom()
 path.drawIt()
 
 _resetButton.onclick = () => {
-
+    ctx.clearRect(0, 0, 500, 500)
+    path.fillRandom()
+    path.drawIt()
 }
 
 _iterateButton.onclick = () => {
