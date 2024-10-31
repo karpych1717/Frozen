3d397adc43413845c00fc61774b352491b93a405
karpych1717 <admkarpych1717@gmail.com>
2024-10-31 11:40:55 +0200

    back to origins

--- short/hometask_vector/normal/Vector.js
+++ short/hometask_vector/normal/Vector.js
@@ -1,55 +1,80 @@
 class Vector {
-    constructor(x, y, system = 'Cartesian') {
-        if (system === 'Cartesian') {
-            this.x = x
-            this.y = y
-        } else {
-            this.x = x * Math.cos(y)
-            this.y = x * Math.sin(y)
-            this.x = Math.round(this.x * 100) / 100
-            this.y = Math.round(this.y * 100) / 100
-        }
+  x
+  y
+
+  constructor (a, b, mode = 'Cartesian') {
+    if (mode === 'Cartesian') {
+      this.x = a
+      this.y = b
+
+      return
     }
 
-    get abs() {
-        return Math.sqrt(this.x ** 2 + this.y ** 2)
+    if (mode === 'Polar') {
+      this.x = a * Math.cos(b)
+      this.y = a * Math.sin(b)
+
+      this.x = Math.round(this.x * 100) / 100
+      this.y = Math.round(this.y * 100) / 100
+
+      return
+    }
+    
+    throw new Error('unknown coordinate system')
+  }
+
+  get abs () {
+    return Math.sqrt(this.x**2 + this.y**2)
+  }
+
+  get angle () {
+    const angle = Math.atan(this.y / this.x);
+    
+    if (this.x >= 0 && this.y >= 0) return angle
+    if (this.x >= 0 && this.y < 0) return angle
+    if (this.x < 0 && this.y >= 0) return angle + Math.PI
+    if (this.x < 0 && this.y < 0) return angle - Math.PI
+  }
+
+  add (...terms) {
+    for (const term of terms) {
+      if (term instanceof Vector) {
+        this.x += term.x
+        this.y += term.y
+        continue
+      }
+      
+      throw new Error('term is not a Vector')
     }
 
-    get angle() {
-        const angle = Math.atan(this.y / this.x)
-        if (this.x >= 0 && this.y >= 0) return angle
-        if (this.x >= 0 && this.y < 0) return angle
-        if (this.x < 0 && this.y >= 0) return angle + Math.PI
-        if (this.x < 0 && this.y < 0) return angle - Math.PI
-    }
+    return this
+  }
 
-    add(...vectors) {
-        let vect = new Vector(this.x, this.y)
-        for (const vector of vectors) {
-            if (vector instanceof Vector) {
-                vect.x += vector.x
-                vect.y += vector.y
-            }
-        }
-        return vect
+  multyplyNumber (multiplyer) {
+    if (typeof multiplyer === 'number') {
+      return new Vector(multiplyer * this.x, multiplyer * this.y)
     }
+    
+    throw new Error('multiplyer is not a number')
+  }
 
-    multyplyScalar(vector) {
-        return this.x * vector.x + this.y * vector.y
+  multyplyScalar (multiplyer) {
+    if (multiplyer instanceof Vector) {
+      return this.x * multiplyer.x + this.y * multiplyer.y
     }
+    
+    throw new Error('multiplyer is not a Vector')
+  }
 
-    multyplyNumber(num) {
-        let vect = new Vector(this.x, this.y)
-        vect.x = this.x * num
-        vect.y = this.y * num
-        return vect
+  multyplyVector (multiplyer) {
+    if (multiplyer instanceof Vector) {
+      return this.abs * multiplyer.abs * Math.sin(this.angle - multiplyer.angle)
     }
+    
+    throw new Error('multiplyer is not a Vector')
+  }
 
-    multyplyVector(vector) {
-        return this.abs * vector.abs * Math.sin(this.angle - vector.angle) // should be cos?
-    }
-
-    get string() {
-        return `(${this.x}, ${this.y})`
-    }
+  get string () {
+    return `(${this.x}, ${this.y})`
+  }
 }
