31ec90d154ee1aad1a9a0ab0a6a3560bc0e18887
PeterMoroz17 <peter.moroz17@gmail.com>
2024-11-13 18:56:00 +0200

    path2 implemented

--- long/least_action/main.js
+++ long/least_action/main.js
@@ -4,12 +4,16 @@ let path = new Path(0, 0, 500, 500)
 path.fillRandom()
 path.drawIt()
 
+let path2 = path.clone()
+
 const candidates = new Array (population_size)
 
 _resetButton.onclick = () => {
     ctx.clearRect(0, 0, 500, 500)
     path.fillRandom()
     path.drawIt()
+    
+    path2 = path.clone()
 }
 
 _iterateButton.onclick = () => {
@@ -19,6 +23,16 @@ _iterateButton.onclick = () => {
   path.drawIt()
 }
 
+_graph.onclick = () => {
+  optimiseStep()
+  optimiseStepOld ()
+
+  path.color = 'white'
+  path.drawIt()
+
+  path2.color = 'gray'
+  path2.drawIt()
+}
 
 function optimiseStep () {
   length_old = path.length
@@ -43,5 +57,20 @@ function optimiseStep () {
     candidates[i].color = almostRandomColor(50, 150)
     candidates[i].drawIt()
   }
-  console.log(point_count - point_count_old, path.length - length_old)
+  console.log("new:", point_count - point_count_old, path.length - length_old)
+}
+
+function optimiseStepOld () {
+  length_old = path2.length
+  point_count_old = point_count
+
+  for (let i = 0; i < population_size; i++) {
+    const candidate = path.clone()
+    candidate.mutate()
+    if (path2.length > candidate.length) {
+      path2 = candidate
+    }
+  }
+  
+  console.log("old:", point_count - point_count_old, path2.length - length_old)
 }
