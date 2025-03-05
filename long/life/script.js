ff9c4201da81cfda42dfd2fae7593607d288d8a6
karpych1717 <admkarpych1717@gmail.com>
2025-03-05 17:52:09 +0200

    create life

---
+++ long/life/script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+_canvas.width = 750
+_canvas.height = 500
+_canvas.style.background = 'gray'
+_canvas.style.border = '3px solid black'
+
+document.body.style.display = 'flex'
+document.body.style.justifyContent = 'center'
+
+const context = _canvas.getContext('2d')
+
