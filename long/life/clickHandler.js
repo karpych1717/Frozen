ad975a1b2f3d074614864c879311d7a0d0921850
PeterMoroz17 <peter.moroz17@gmail.com>
2025-03-26 19:17:34 +0200

    button click fixed

--- long/life/clickHandler.js
+++ long/life/clickHandler.js
@@ -1,10 +1,12 @@
-import Button from "./Button"
+import Button from "./Button.js"
+import { pause } from "./main.js"
 
 function clickHandler(event) {
   const x = event.offsetX
   const y = event.offsetY
 
   alert(x + " " + y)
+  if (pause.isOverIt(x, y)) pause.onclick()
 }
 
 export default clickHandler
--- long/life/main.js
+++ long/life/main.js
@@ -3,7 +3,6 @@
 import simpleFunction from './sample.js'
 import Button from './Button.js'
 import clickHandler from './clickHandler.js'
-document.onclick = clickHandler
 
 _canvas.width = 750
 _canvas.height = 500
@@ -15,8 +14,10 @@ document.body.style.justifyContent = 'center'
 
 const context = _canvas.getContext('2d')
 
-const pause = new Button(700, 50, 50, 50, () => alert("Hello world"))
+export const pause = new Button(700, 50, 50, 50, () => alert("Hello world"))
 pause.drawIt(context)
 
 // const field = new Field (...)
 // const togglePauseButton = new Button(200, 300, 30, 30, () => field.togglePause() )
+
+document.onclick = clickHandler
