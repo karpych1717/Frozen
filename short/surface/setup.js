428cb9b02da76f51b4ab549447e7737fe2239d59
PeterMoroz17 <peter.moroz17@gmail.com>
2025-05-21 18:48:42 +0300

    added surface interaction

--- short/surface/setup.js
+++ short/surface/setup.js
@@ -81,3 +81,69 @@ function drawBalls () {
     arr[i].drawIt()
   }
 }
+
+class Vector {
+  constructor (x, y) {
+    this.x = x
+    this.y = y
+  }
+}
+
+function fIndividual(idx, arr) {
+  let answer = surface(arr[idx].x, arr[idx].y)
+  /*for (let i = 0; i < arr.length; i++) {
+      if (i == idx) continue
+      answer += Math.max(0,
+          arr[idx].r + arr[i].r - Math.sqrt(
+              (arr[idx].x - arr[i].x) ** 2 +
+              (arr[idx].y - arr[i].y) ** 2
+          )
+      )
+  }*/
+  return answer
+}
+
+function f(arr) {
+  let answer = 0
+  for (let i = 0; i < arr.length; i++) {
+      answer += fIndividual(i, arr)
+  }
+  return answer
+}
+
+function fDerivativeIndividual(idx, dx, dy, arr) {
+  const fCallX1 = f(arr)
+  arr[idx].x += dx
+  const fCallX2 = f(arr)
+  arr[idx].x -= dx
+  const fCallY1 = f(arr)
+  arr[idx].y += dy
+  const fCallY2 = f(arr)
+  arr[idx].y -= dy
+  return new Vector((fCallX2 - fCallX1) / dx, (fCallY2 - fCallY1) / dy)
+}
+
+function fDerivative(dx, dy, arr) {
+  const D = new Array(arr.length)
+  for (let i = 0; i < arr.length; i++) {
+      D[i] = fDerivativeIndividual(i, dx, dy, arr)
+  }
+  return D
+}
+
+function cycle(dx, dy, arr) {
+  const D = fDerivative(dx, dy, arr)
+  for (let i = 0; i < arr.length; i++) {
+      arr[i].x -= D[i].x
+      arr[i].y -= D[i].y
+  }
+}
+
+document.onpointerdown = () => {
+  for (let i = 0; i < 1; i++) {
+      cycle(10, 10, arr)
+  }
+  context.clearRect(0, 0, 500, 500)
+  drawSurface()
+  drawBalls()
+}
