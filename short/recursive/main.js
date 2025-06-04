d9102934f37a97f2565edec2210aaffb59dce2a6
PeterMoroz17 <peter.moroz17@gmail.com>
2025-06-04 18:58:57 +0300

    added uniqueRandomFillArray

--- short/recursive/main.js
+++ short/recursive/main.js
@@ -34,8 +34,32 @@ function countAverageArray (x, len) {
     }
 }
 
-inputArray(0, 10)
+function getRandom(amount) {
+    return Math.floor(Math.random() * amount);
+}
+
+function uniqueRandomFillArray (x, len, used) {
+    if (x >= len) return 0
+    if (x == 0) {
+        const used0 = new Array(len).fill(false)
+        arr[0] = getRandom(len)
+        used0[arr[0]] = true
+        uniqueRandomFillArray(x + 1, len, used0)
+    } else {
+        arr[x] = getRandom(len)
+        while (used[arr[x]]) {
+            arr[x] = getRandom(len)
+        }
+        used[arr[x]] = true
+        uniqueRandomFillArray(x + 1, len, used)
+    }
+}
+
+//inputArray(0, 10)
 
 //outputArray(0, 10)
+
+uniqueRandomFillArray(0, 10)
+outputArray(0, 10)
 
 console.log(countAverageArray(0, 10))
