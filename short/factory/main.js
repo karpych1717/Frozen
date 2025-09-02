8fb6756a377c7c36e1db546bd4b73aaaa7b86f96
PeterMoroz17 <peter.moroz17@gmail.com>
2025-09-02 10:47:31 +0300

    hometask done

--- short/factory/main.js
+++ short/factory/main.js
@@ -6,21 +6,46 @@ document.body.style.justifyContent = 'center'
 
 
 function factory() {
-    let x = 0
+    let last = Date.now()
     function count() {
-        console.log(x)
-        x += 1
+        console.log((Date.now() - last) / 1000)
+        last = Date.now()
     }
     return count
 }
 
+class B {
+    constructor() {
+        this.lastClick = Date.now()
+    }
+
+    handleClick() {
+        console.log((Date.now() - this.lastClick) / 1000)
+        this.lastClick = Date.now()
+    }
+}
+
+class C {
+    constructor(id) {
+        this.button = document.getElementById(id)
+        this.button.onclick = () => this.handleClick()
+        this.lastClick = Date.now()
+    }
+
+    handleClick() {
+        console.log((Date.now() - this.lastClick) / 1000)
+        this.lastClick = Date.now()
+    }
+}
+
 let a = factory()
-let b = factory()
+let b = new B()
+let c = new C('_Button3')
 
 _Button1.onclick = () => {
     a()
 }
 
 _Button2.onclick = () => {
-    b()
+    b.handleClick()
 }
