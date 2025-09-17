bae2cfbca3443e652b9c8c7c82d6e823d3bc2f8e
PeterMoroz17 <peter.moroz17@gmail.com>
2025-09-17 17:15:39 +0300

    redone data storage

--- short/curves/classes.js
+++ short/curves/classes.js
@@ -65,18 +65,18 @@ class Curve {
 
 class Transform {
     constructor (n) {
-        this.n = n // transform array length
-        this.N = 2 * this.n + 1 // data length
-        // data[0] = a_0, data[1, n] = A, data[n+1, 2*n] = B
-        this.data = new Array(this.N).fill(0)
+        this.n = n
+        this.a0 = 0
+        this.a = new Array(this.n).fill(0)
+        this.b = new Array(this.n).fill(0)
         this.h = 0.1   // parameter shift
         this.lr = 0.03 // learning rate
     }
 
     make_transform(x) {
-        let ans = this.data[0]
+        let ans = this.a0
         for (let i = 0; i < this.n; i += 1) {
-            ans += this.data[i+1] * Math.cos(Math.PI * i * x) + this.data[i + this.n + 1] * Math.sin(Math.PI * i * x)
+            ans += this.a[i] * Math.cos(Math.PI * i * x) + this.b[i] * Math.sin(Math.PI * i * x)
         }
         return ans
     }
@@ -85,7 +85,7 @@ class Transform {
         let ans = 0;
         let vi
         for (let i = 0; i < curve.n; i += 1) {
-            vi = (this.make_transform(curve.arr[i].x) + curve.arr[i].y)
+            vi = (this.make_transform(curve.arr[i].x) - curve.arr[i].y)
             ans += vi * vi
         }
         ans /= this.n
@@ -93,25 +93,57 @@ class Transform {
     }
 
     iterate(curve) {
-        let gradient = new Array(this.N).fill(0)
+        let gradient = new Array(1 + this.n * 2).fill(0)
 
-        let original_value, decreaced_result, increased_result
-        for (let i = 0; i < this.N; i += 1) {
-            original_value = this.data[i]
+        let original_value, result_decreaced, result_increased
 
-            this.data[i] = original_value - this.h
-            decreaced_result = this.evaluate_fitness(curve)
 
-            this.data[i] = original_value + this.h
-            increased_result = this.evaluate_fitness(curve)
+        original_value = this.a0
 
-            gradient[i] = (increased_result - decreaced_result) / (2 * this.h)
+        this.a0 = original_value - this.h
+        result_decreaced = this.evaluate_fitness(curve)
+
+        this.a0 = original_value + this.h
+        result_increased = this.evaluate_fitness(curve)
+
+        this.a0 = original_value
+
+        gradient[0] = (result_increased - result_decreaced) / (2 * this.h)
+
+        for (let i = 0; i < this.n; i += 1) {
+            original_value = this.a[i]
+
+            this.a[i] = original_value - this.h
+            result_decreaced = this.evaluate_fitness(curve)
+
+            this.a[i] = original_value + this.h
+            result_increased = this.evaluate_fitness(curve)
+
+            this.a[i] = original_value
+
+            gradient[i + 1] = (result_increased - result_decreaced) / (2 * this.h)
+        }
+        
+        for (let i = 0; i < this.n; i += 1) {
+            original_value = this.b[i]
+
+            this.b[i] = original_value - this.h
+            result_decreaced = this.evaluate_fitness(curve)
+
+            this.b[i] = original_value + this.h
+            result_increased = this.evaluate_fitness(curve)
+
+            this.b[i] = original_value
             
-            this.data[i] = original_value
+            gradient[i + this.n + 1] = (result_increased - result_decreaced) / (2 * this.h)
         }
 
-        for (let i = 0; i < this.N; i += 1) {
-            this.data[i] -= this.lr * gradient[i]
+        this.a0 -= this.lr * gradient[0]
+        for (let i = 0; i < this.n; i += 1) {
+            this.a[i] -= this.lr * gradient[i + 1]
+        }
+        for (let i = 0; i < this.n; i += 1) {
+            this.b[i] -= this.lr * gradient[i + this.n + 1]
         }
     }
 
@@ -121,13 +153,13 @@ class Transform {
         context.beginPath()
         context.moveTo(
             decypher(curve.arr[0].x, curve.w),
-            decypher(this.make_transform(curve.arr[0].x, this.data), curve.h)
+            decypher(-this.make_transform(curve.arr[0].x), curve.h)
         )
         
         for (let i = 1; i < curve.n; i++) {
             context.lineTo(
                 decypher(curve.arr[i].x, curve.w),
-                decypher(this.make_transform(curve.arr[i].x, this.data), curve.h)
+                decypher(-this.make_transform(curve.arr[i].x), curve.h)
             )
         }
         context.stroke()
