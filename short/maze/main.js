af2738763dcc27dd3d71099ce58cf5f4dd87f0ad
PeterMoroz17 <peter.moroz17@gmail.com>
2025-09-17 18:21:49 +0300

    made random

--- short/maze/main.js
+++ short/maze/main.js
@@ -32,18 +32,32 @@ for (let i = 0; i < 25; i += 1) {
 }
 
 field.field[0][12].val = 0
+field.field[1][12].val = 0
 
 function dfs(x, y) {
     if (used[x][y] == 1) return
     used[x][y] = 1
     field.field[x][y].val = 0
+    if (x == 1 || y == 1 || x == 23 || y == 23) return
 
-    if (x != 1 && used[x-2][y] + used[x-1][y-1] + used[x-1][y+1]== 0) dfs(x-1, y)
-    if (x != 24 && used[x+2][y] + used[x+1][y-1] + used[x+1][y+1] == 0) dfs(x+1, y)
-    if (y != 1 && used[x][y-2] + used[x-1][y-1] + used[x+1][y-1] == 0) dfs(x, y-1)
-    if (y != 24 && used[x][y+2] + used[x-1][y+1] + used[x+1][y+1] == 0) dfs(x, y+1)
+    let permutation = new Array(0, 1, 2, 3)
+    for (let i = 4; i > 1; i--) {
+        let rand = Math.floor(Math.random() * i)
+        permutation.sort()
+        //if (x == 1 && y == 12) console.log(rand, permutation[rand])
+        if (permutation[rand] == 0) {
+            if (used[x-2][y] + used[x-2][y-1] + used[x-2][y+1] + used[x-1][y-1] + used[x-1][y+1] == 0) dfs(x-1, y)
+        } else if (permutation[rand] == 1) {
+            if (used[x+2][y] + used[x+2][y-1] + used[x+2][y+1] + used[x+1][y-1] + used[x+1][y+1] == 0) dfs(x+1, y)
+        } else if (permutation[rand] == 2) {
+            if (used[x][y-2] + used[x-1][y-2] + used[x+1][y-2] + used[x-1][y-1] + used[x+1][y-1] == 0) dfs(x, y-1)
+        } else {
+            if (used[x][y+2] + used[x-1][y+2] + used[x+1][y+2] + used[x-1][y+1] + used[x+1][y+1] == 0) dfs(x, y+1)
+        }
+        permutation[rand] = 1000
+    }
 }
 
-dfs(1, 12)
+dfs(2, 12)
 
 field.drawIt(context)
