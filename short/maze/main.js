927ac077f19066fa152c681ca0ce95239a7886e8
PeterMoroz17 <peter.moroz17@gmail.com>
2025-10-01 18:10:52 +0300

    fixed sort bug

--- short/maze/main.js
+++ short/maze/main.js
@@ -31,62 +31,42 @@ for (let i = 0; i < 25; i += 1) {
     field.field[i][24].val = 100
 }
 
+let last = new Array(25)
+for (let i = 0; i < 25; i += 1) {
+    last[i] = new Array(25).fill(0)
+}
+
 field.field[0][12].val = 0
 field.field[1][12].val = 0
 
+function isDiggable(x, y) {
+    if (x == 1 || y == 1 || x == 23 || y == 23) return false
+    if (used[x][y]) return false
+    if (used[x-1][y] + used[x+1][y] + used[x][y+1] + used[x][y-1] > 1) {
+        return false
+    }
+    if (used[x-1][y]) if (used[x+1][y+1] || used[x+1][y-1]) return false
+    if (used[x][y-1]) if (used[x+1][y+1] || used[x-1][y+1]) return false
+    if (used[x+1][y]) if (used[x-1][y+1] || used[x-1][y-1]) return false
+    if (used[x][y+1]) if (used[x+1][y-1] || used[x-1][y-1]) return false
+
+    return true
+}
+
 function dfs(x, y) {
-    if (used[x][y] == 1) return
+    if (used[x][y] == 1 || !isDiggable(x, y)) return 
     used[x][y] = 1
     field.field[x][y].val = 0
-    if (x == 1 || y == 1 || x == 23 || y == 23) return
 
     let permutation = new Array(0, 1, 2, 3)
     for (let i = 4; i > 0; i--) {
         let rand = Math.floor(Math.random() * i)
-        permutation.sort()
         //if (x == 1 && y == 12) console.log(rand, permutation[rand])
-        if (permutation[rand] == 0) {
-            if (
-                used[x-2][y] +
-                used[x-2][y-1] +
-                used[x-2][y+1] +
-                used[x-1][y-1] +
-                used[x-1][y+1] == 0
-            ) {
-                dfs(x-1, y)
-            }
-        } else if (permutation[rand] == 1) {
-            if (
-                used[x+2][y] +
-                used[x+2][y-1] +
-                used[x+2][y+1] +
-                used[x+1][y-1] +
-                used[x+1][y+1] == 0
-            ) {
-                dfs(x+1, y)
-            }
-        } else if (permutation[rand] == 2) {
-            if (
-                used[x][y-2] +
-                used[x-1][y-2] +
-                used[x+1][y-2] +
-                used[x-1][y-1] +
-                used[x+1][y-1] == 0
-            ) {
-                dfs(x, y-1)
-            }
-        } else {
-            if (
-                used[x][y+2] +
-                used[x-1][y+2] +
-                used[x+1][y+2] +
-                used[x-1][y+1] +
-                used[x+1][y+1] == 0
-            ) {
-                dfs(x, y+1)
-            }
-        }
-        permutation[rand] = 1000
+        if (permutation[rand] == 0 && isDiggable(x-1, y)) dfs(x-1, y)
+        if (permutation[rand] == 1 && isDiggable(x+1, y)) dfs(x+1, y)
+        if (permutation[rand] == 2 && isDiggable(x, y-1)) dfs(x, y-1)
+        if (permutation[rand] == 3 && isDiggable(x, y+1)) dfs(x, y+1)
+        permutation.splice(rand, 1)
     }
 }
 
