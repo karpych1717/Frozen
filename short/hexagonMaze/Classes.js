6b79f976b74672c22822b411a55666416fd5d2d8
PeterMoroz17 <peter.moroz17@gmail.com>
2025-10-14 17:00:42 +0300

    hometask done

--- short/hexagonMaze/Classes.js
+++ short/hexagonMaze/Classes.js
@@ -15,6 +15,13 @@ class Circle {
   }
 }
 
+class Vector {
+  constructor (x, y) {
+    this.x = x
+    this.y = y
+  }
+}
+
 class Hexagon {
   constructor (x, y, h, c) {
     this.x = x
@@ -26,11 +33,11 @@ class Hexagon {
   drawIt (context) {
     context.beginPath()
     context.moveTo(this.x - this.h / 2, this.y)
-    context.lineTo(this.x - this.h / 4, this.y - this.h / 2)
-    context.lineTo(this.x + this.h / 4, this.y - this.h / 2)
+    context.lineTo(this.x - this.h / 4, this.y - this.h * Math.sqrt(3) / 4)
+    context.lineTo(this.x + this.h / 4, this.y - this.h * Math.sqrt(3) / 4)
     context.lineTo(this.x + this.h / 2, this.y)
-    context.lineTo(this.x + this.h / 4, this.y + this.h / 2)
-    context.lineTo(this.x - this.h / 4, this.y + this.h / 2)
+    context.lineTo(this.x + this.h / 4, this.y + this.h * Math.sqrt(3) / 4)
+    context.lineTo(this.x - this.h / 4, this.y + this.h * Math.sqrt(3) / 4)
     context.lineTo(this.x - this.h / 2, this.y)
     context.fillStyle = this.c;
     context.fill()
@@ -38,4 +45,97 @@ class Hexagon {
   }
 }
 
-export default Hexagon
+class Field {
+  constructor (hexagonWidth, x, y, w, h) {
+    this.hexagonWidth = hexagonWidth
+    this.x = x
+    this.y = y
+    this.w = w
+    this.h = h
+
+    this.hexagonHeight = this.hexagonWidth * Math.sqrt(3) / 2
+
+    this.n = Math.floor(4 * this.w / 3 / this.hexagonWidth)
+    if (this.n == 1) {
+      this.m = Math.floor(this.h / this.hexagonHeight)
+    } else {
+      this.m = Math.floor(this.h / this.hexagonHeight - 1 / 2)
+      if (this.m == 0) {
+        this.m = Math.floor(this.h / this.hexagonHeight)
+      }
+    }
+
+    this.dx = (this.w - this.hexagonWidth - this.hexagonWidth * 3 / 4 * (this.n - 1)) / 2
+    if (this.n == 1) {
+      this.dy = (this.h - this.hexagonHeight * this.m) / 2
+    } else {
+      this.dy = (this.h - this.hexagonHeight * this.m - this.hexagonHeight / 2) / 2
+    }
+
+    this.field = new Array(this.n)
+    for (let i = 0; i < this.n; i++) {
+      this.field[i] = new Array(this.m)
+      for (let j = 0; j < this.m; j++) {
+        this.field[i][j] = new Hexagon(
+          this.dx + this.hexagonWidth / 2 + i * this.hexagonWidth * 3 / 4,
+          this.dy + this.hexagonHeight / 2 + j * this.hexagonHeight,
+          this.hexagonWidth,
+          `grey`
+        )
+        if (i % 2 == 1) this.field[i][j].y += this.hexagonHeight / 2
+      }
+    }
+  }
+
+  drawIt(context) {
+    for (let i = 0; i < this.n; i++) {
+      for (let j = 0; j < this.m; j++) {
+        this.field[i][j].drawIt(context)
+      }
+    }
+  }
+
+  setColorByVal(x, y, color) {
+    if (x < 0 || y < 0 || x > this.n || y > this.m) {
+      console.log("ERROR SetColor Boundary")
+      return -1
+    }
+    this.field[x][y].c = `rgb(${color}, ${color}, ${color})` 
+  }
+
+  getNeighbours(x, y) {
+    let ret = new Array()
+
+    if (x != 0) {
+      ret.push(new Vector(x-1, y))
+    }
+    if (x != this.n - 1) {
+      ret.push(new Vector(x+1, y))
+    }
+    if (y != 0) {
+      ret.push(new Vector(x, y-1))
+    }
+    if (y != this.m - 1) {
+      ret.push(new Vector(x, y+1))
+    }
+    if (x % 2 == 0) {
+      if (x != 0 && y != 0) {
+        ret.push(new Vector(x-1, y-1))
+      }
+      if (x != this.n - 1 && y != 0) {
+        ret.push(new Vector(x+1, y-1))
+      }
+    } else {
+      if (x != 0 && y != this.m - 1) {
+        ret.push(new Vector(x-1, y+1))
+      }
+      if (x != this.n - 1 && y != this.m - 1) {
+        ret.push(new Vector(x+1, y+1))
+      }
+    }
+    
+    return ret
+  }
+}
+
+export default Field
--- short/hexagonMaze/main.js
+++ short/hexagonMaze/main.js
@@ -1,6 +1,6 @@
 /* global _canvas */
 'use strict'
-import Hexagon from './Classes.js'
+import Field from './Classes.js'
 
 _canvas.width = 500
 _canvas.height = 500
@@ -12,5 +12,53 @@ document.body.style.justifyContent = 'center'
 
 const context = _canvas.getContext('2d')
 
-const test = new Hexagon(250, 250, 16, "blue")
-test.drawIt(context)
+const field = new Field(16, 0, 0, 500, 500)
+
+let used = new Array(field.n)
+for (let i = 0; i < field.n; i++) used[i] = new Array(field.m)
+
+let maze = generator(0, 0)
+field.drawIt(context)
+    
+_iterateButton.onclick = () => {
+    if (maze.next().done) console.log("done")
+    field.drawIt(context)
+}
+
+function isDiggable(x, y) {
+    let neighbours = field.getNeighbours(x, y)
+    let count = 0
+    for (let i = 0; i < neighbours.length; i++) {
+        if (used[neighbours[i].x][neighbours[i].y] == 1) {
+            count++
+        }
+    }
+    if (count > 1) return false
+    return true
+}
+
+function* generator(x, y) {
+    used[x][y] = true
+    field.setColorByVal(x, y, 0)
+    yield
+    
+    let neighbours = field.getNeighbours(x, y)
+
+    let option = new Array()
+    for (let i = 0; i < neighbours.length; i++) option.push(i)
+    
+    for (let i = neighbours.length; i > 0; i--) {
+        let rand = Math.floor(Math.random() * i)
+        if (isDiggable(
+            neighbours[option[rand]].x,
+            neighbours[option[rand]].y
+        )) {
+            let maze = generator(
+                neighbours[option[rand]].x,
+                neighbours[option[rand]].y
+            )
+            while (!maze.next().done) yield
+        }
+        option.splice(rand, 1)
+    }
+}
