e645602520c76e9d86f9b6390e288afab106bce0
Peter <peter.moroz17@gmail.com>
2026-04-08 17:41:03 +0300

    update

--- long/maze_wanderer/the_maze_wanderer/classes/Wanderer.js
+++ long/maze_wanderer/the_maze_wanderer/classes/Wanderer.js
@@ -12,34 +12,16 @@ class Wanderer {
     this.decisionThreshould = 0.25
   }
 
-  updateIt(dt) {
+  updateIt() {
     const input = new Array(4)
     for (let i = 0; i < 4; i++) input[i] = new Array(1)
     for (let i = 0; i < 3; i++) input[0][i] = this.square.rays[i].length()
     input[0][3] = this.square.speed()
 
     const decision = this.brain.calculate(new Matrix(4, 1, input))
-    const forward = decision.arr[0][0] > this.decisionThreshould
-    const backward = decision.arr[0][1] > this.decisionThreshould
-    const left = decision.arr[0][2] > this.decisionThreshould
-    const right = decision.arr[0][3] > this.decisionThreshould
 
-    if (forward ^ backward) {
-        this.square.fxR = 0
-    } else if (forward) {
-        this.square.fxR = 0.001
-    } else if (backward){
-        this.square.fxR = -0.001
-    }
-    if (right ^ screenLeft) {
-        this.square.va = 0
-    } else if (right) {
-        this.square.va = 0.005
-    } else if (left) {
-        this.square.va = -0.005
-    }
-
-    //this.square.va = (decision.arr[0][2] - decision.arr[0][3]) * 0.005
+    this.square.fxR = (decision.arr[0][0] - decision.arr[0][1]) * 0.001
+    this.square.va = (decision.arr[0][2] - decision.arr[0][3]) * 0.005
   }
 
   drawIt(context) {
