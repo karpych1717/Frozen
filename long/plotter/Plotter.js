fcea73e8883ef2d8e1d1332a477ffda6e8159399
Peter <peter.moroz17@gmail.com>
2026-08-05 18:43:01 +0300

    made global min max in plotter, bug in formula of y(x)

--- long/plotter/Plotter.js
+++ long/plotter/Plotter.js
@@ -3,39 +3,49 @@ class Plotter {
     this._canvas = _canvas
     this.context = _canvas.getContext('2d')
 
-    this.x = _canvas.width
-    this.y = _canvas.height
+    this.w = _canvas.width
+    this.h = _canvas.height
+    
+    this.minX = 1e9
+    this.maxX = -1e9
+    this.minY = 1e9
+    this.maxY = -1e9
+
+    this.plots = new Array()
   }
 
-  plot (x, y) {
-    let len = x.length
-    let minX = 1e9, maxX = -1e9
-    let minY = 1e9, maxY = -1e9
+  drawIt() {
+    const kx = this.w / (this.maxX - this.minX)
+    const ky = this.h / (this.maxY - this.minY)
 
-    for (let i = 0; i < len; i++) {
-      minX = Math.min(minX, x[i])
-      maxX = Math.max(maxX, x[i])
-      minY = Math.min(minY, y[i])
-      maxY = Math.max(maxY, y[i])
+    for (let j = 0; j < this.plots.length; j++) {
+      this.context.beginPath()
+
+      const x0 = (this.plots[j][0][0] - this.minX) * kx
+      const y0 = (this.plots[j][1][0] - this.minY) * ky
+      this.context.moveTo(x0, y0)
+      for (let i = 1; i < this.plots[j][0].length; i++) {
+        let xi = (this.plots[j][0][i] - this.minX) * kx
+        let yi = (this.plots[j][1][i] - this.minY) * ky
+        this.context.lineTo(xi, yi)
+      }
+
+      this.context.strokeStyle = this.plots[j][2]
+      this.context.stroke()
+    }
+  }
+
+  plot (x, y, color = "Black") {
+    for (let i = 0; i < x.length; i++) {
+      this.minX = Math.min(this.minX, x[i])
+      this.maxX = Math.max(this.maxX, x[i])
+      this.minY = Math.min(this.minY, y[i])
+      this.maxY = Math.max(this.maxY, y[i])
     }
 
-    const kx = this.x / (maxX - minX)
-    const ky = this.y / (maxY - minY)
-
-    this.context.beginPath()
-
-    const x0 = (x[0] - minX) * kx
-    const y0 = (y[0] - minY) * ky
-    this.context.moveTo(x0, y0)
-    for (let i = 1; i < len; i++) {
-      let xi = (x[i] - minX) * kx
-      let yi = (y[i] - minY) * ky
-      this.context.lineTo(xi, yi)
-      console.log(xi, yi)
-    }
-
-    this.context.strokeStyle = "black"
-    this.context.stroke()
+    this.plots.push([x, y, color])
+    this.context.clearRect(0, 0, this.w, this.h)
+    this.drawIt()
   }
 }
 
--- long/plotter/main.js
+++ long/plotter/main.js
@@ -15,4 +15,25 @@ const context = _canvas.getContext('2d')
 
 const plotter = new Plotter(_canvas)
 
-plotter.plot([1, 2, 3, 4], [1, 4, 2, 3])
+const size = 100
+let dx = 1, dy
+let ans1x = new Array(size), ans1y = new Array(size)
+ans1x[0] = 0
+ans1y[0] = 3
+for (let i = 1; i < size; i++) {
+    dy = -2 * ans1y[i-1] * dx
+
+    ans1x[i] = ans1x[i-1] + dx
+    ans1y[i] = ans1y[i-1] + dy
+}
+
+let ans2x = new Array(size), ans2y = new Array(size)
+ans2x[0] = 0
+ans2y[0] = 3
+for (let i = 1; i < size; i++) {
+    ans2x[i] = ans2x[i-1] + dx
+    ans2y[i] = 3 * Math.exp(-2 * ans2x[i])
+}
+
+plotter.plot(ans1x, ans1y, "red")
+plotter.plot(ans2x, ans2y, "blue")
