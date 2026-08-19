a2784ead0736e952dfa678071f4f10ac709b2591
Peter <peter.moroz17@gmail.com>
2026-08-19 15:22:54 +0300

    hometask

--- long/plotter/main.js
+++ long/plotter/main.js
@@ -15,18 +15,18 @@ const context = _canvas.getContext('2d')
 
 const plotter = new Plotter(_canvas)
 
-const size = 100
-const x0 = 0, xMax = 1
-let dx = (xMax - x0 + 1) / size, dy
-const h = 0.001
+const x0 = 0, xMax = 1, N_steps = 100
+const y0 = 5
+let dx = (xMax - x0) / N_steps, dy
+const h = 0.01
 
-let ans1x = new Array(size), ans1y = new Array(size)
+let ans1x = new Array(N_steps + 1), ans1y = new Array(N_steps + 1)
 ans1x[0] = x0
-ans1y[0] = 0
-for (let i = 1; i < size; i++) {
+ans1y[0] = y0
+for (let i = 1; i <= N_steps; i++) {
     let y = ans1y[i-1]
     for (let t = 0; t < dx; t += h) {
-        dy = (y + Math.sin((ans1x[i-1] + t) * 25)) * h
+        dy = (y + 100 * Math.cos(10 * (ans1x[i-1] + t)) - 10 * Math.sin(10 * (ans1x[i-1] + t))) * h
         y = y + dy
     }
 
@@ -34,15 +34,14 @@ for (let i = 1; i < size; i++) {
     ans1y[i] = y
 }
 
-let ans2x = new Array(size), ans2y = new Array(size)
-ans2x[0] = x0
-ans2y[0] = 0
-for (let i = 1; i < size; i++) {
-    ans2x[i] = ans2x[i-1] + dx
-    ans2y[i] = (25/626) * Math.exp(ans2x[i]) - (1/626) * Math.sin(ans2x[i]*25) - (25/626) * Math.cos(ans2x[i]*25)
+const analyticalPoints = 100
+let ans2x = new Array(analyticalPoints), ans2y = new Array(analyticalPoints)
+for (let i = 0; i < analyticalPoints; i++) {
+    ans2x[i] = x0 + (xMax - x0) * i / (analyticalPoints - 1)
+    ans2y[i] = 5 * Math.exp(ans2x[i]) + 10 * Math.sin(10 * ans2x[i])
 }
 
 plotter.plot(ans1x, ans1y, "red")
 plotter.plot(ans2x, ans2y, "blue")
 
-console.log("x0:", x0, "xMax:", xMax, "N:", size)
+console.log("x0:", x0, "xMax:", xMax, "N_steps:", N_steps)
